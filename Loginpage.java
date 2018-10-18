e5e400aa65fcc6d712abf7cc97a52afa13b91977
samariwa <42217461+samariwa@users.noreply.github.com>
2018-10-18 21:02:46 +0300

    Add files via upload
    login page with the usernames and passwords set

--- Loginpage.java
+++ Loginpage.java
@@ -8,13 +8,14 @@ package matatu.system;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
+import java.util.Arrays;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
-
+import javax.swing.*;
 /**
  *
  * @author samue
@@ -57,7 +58,7 @@ public class Loginpage
         a.add(login);
         loginpage.add(a);
         //adding an actionlistener for the login button
-        login.addActionListener((java.awt.event.ActionEvent gothrough)->{goactionPerformed(gothrough);});
+        login.addActionListener((java.awt.event.ActionEvent editorgothrough)->{goactionPerformed(editorgothrough);});
         //adding an actionlistener for the clear button
         clear.addActionListener((java.awt.event.ActionEvent clean)->{clearactionPerformed(clean);});
         //layout setup
@@ -78,8 +79,41 @@ public class Loginpage
         nametext.setText("");
         passwordtext.setText("");    
     }  
-        public void goactionPerformed(ActionEvent gothrough)
+        public void goactionPerformed(ActionEvent editorgothrough)
         {
-            homepage home=new homepage();
+         char[] p = passwordtext.getPassword();
+         String u = nametext.getText();
+         String secret = "editsecret1"; 
+         char sc[]= secret.toCharArray();
+         char []q=passwordtext.getPassword();
+         String v=nametext.getText();
+         String secret1="readsecret";
+         char xy[]=secret1.toCharArray();
+         char []r=passwordtext.getPassword();
+         String w=nametext.getText();
+         String secret2="editsecret2";
+         char yz[]=secret2.toCharArray();
+         char []s=passwordtext.getPassword();
+         String x=nametext.getText();
+         String secret3="editsecret3";
+         char za[]=secret3.toCharArray();
+         if(Arrays.equals(sc,p) && u.equals("editor1") )
+         {
+             homepage home=new homepage();
+         }
+         else if(Arrays.equals(xy,q)&&v.equals("reader"))
+         {
+             homepagereadonly read=new homepagereadonly();
+         }
+         else if(Arrays.equals(yz,s)&&v.equals("editor2"))
+         {
+             homepage read=new homepage();
+         }
+         else if(Arrays.equals(za,s)&&v.equals("editor3"))
+         {
+             homepage read=new homepage();
+         }
+         else
+             JOptionPane.showMessageDialog(null,"Invalid Passord or Username","Alert",JOptionPane.WARNING_MESSAGE);
         }
 }
